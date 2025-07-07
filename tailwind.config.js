00cfa7fc1b2565875efb34832e33621bc63cd6c1
AhmerAli1813 <AhmerAli1813@gmail.com>
2025-07-07 10:10:02 +0500

    Publish on NPM

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#1d4ed8',
+        secondary: '#64748b',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
